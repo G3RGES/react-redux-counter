90b99677f4cbd57f470f7870a09e29b9a2ab0622
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-17 01:36:25 +0300

    UPD APP.js & index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-// import store from "./store";
+import store from "./store";
+import { Provider } from "react-redux";
 
 import Counter from "./component/Counter";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Counter />
-    </>
+    </Provider>
   );
 }
 
